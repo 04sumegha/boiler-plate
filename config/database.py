34d06c07703d7443b08993c55bf732508d0a4270
ms office <sumeghagupta455@gmail.com>
2025-03-26 13:10:32 +0530

    added connection pooling

--- config/database.py
+++ config/database.py
@@ -4,7 +4,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:udaysingh@localhost/boilerplate")
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_size = 10,
+    max_overflow = 5,
+    pool_timeout = 120
+)
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 Base = declarative_base()
 
